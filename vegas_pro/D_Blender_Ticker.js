4438fd809a7ac7eaef3d44f60f5fff1f816f1c4f
Mix3r_Durachok <verysoft@yandex.ru>
2024-11-12 17:44:08 +0500

    Adding can opener feature into Blender Ticker btn

--- vegas_pro/D_Blender_Ticker.js
+++ vegas_pro/D_Blender_Ticker.js
@@ -3,20 +3,28 @@ new school
  **/ 
 
 import System;
+import System.Text;
 import System.IO;
-import System.Object;
 import System.Windows.Forms;
 import Sony.Vegas;
+import ScriptPortal.Vegas;
 
-var ttl_length = new Timecode(5200);
+var sFFMpegPath = "\""+System.Environment.GetFolderPath(System.Environment.SpecialFolder.ProgramFiles)+"\\Totalcmd_p\\FFMPEG\\ffmpeg.exe\"";
+var tStrmLength = null;
+var newsGroup = null;
+var bFlag_DoneV = null;
+var bFlag_DoneA = null;
+var tEventLoc = Vegas.Transport.CursorPosition;
 
 try
 {
-	var Titlebgtrack = FindTrack("Ticker");
-	if (null == Titlebgtrack) {
-           Titlebgtrack = new VideoTrack(0, "Ticker");
-           Vegas.Project.Tracks.Add(Titlebgtrack);
-	}
+    if (System.Windows.Forms.Control.ModifierKeys == Keys.Shift) {
+        var ttl_length = new Timecode(5200);
+	    var Titlebgtrack = FindTrack("Ticker");
+	    if (null == Titlebgtrack) {
+            Titlebgtrack = new VideoTrack(0, "Ticker");
+            Vegas.Project.Tracks.Add(Titlebgtrack);
+	    }
         var evntEnum = new Enumerator(Titlebgtrack.Events);
         //var ev_edit = 0;
         while (!evntEnum.atEnd()) {
@@ -39,12 +47,107 @@ try
               evntEnum.moveNext();
         }
         CreateGeneratedMedia(Titlebgtrack,"Narrator_Ticker_1");
+    } else {
+        CanOpener();
+    }
 }
+
 catch (errorMsg)
 {
-        if (errorMsg != 1) {
-	   MessageBox.Show(errorMsg, "Error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+    if (errorMsg != 1) {
+	    MessageBox.Show(errorMsg, "Error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+    }
+}
+
+function CanOpener() {
+try {
+    var fullpath_all = GetVideoDialog();
+
+    for (var tTmpTrk in Vegas.Project.Tracks) {
+        if (tTmpTrk.Events.Count > 0) {
+            if (tTmpTrk.Events[tTmpTrk.Events.Count-1].Start+tTmpTrk.Events[tTmpTrk.Events.Count-1].Length > tEventLoc) {
+                tEventLoc = tTmpTrk.Events[tTmpTrk.Events.Count-1].Start+tTmpTrk.Events[tTmpTrk.Events.Count-1].Length;
+            }
         }
+    }
+
+    for (var fullpath_N in fullpath_all) {
+        tStrmLength = null;
+        newsGroup = null;
+        bFlag_DoneV = null;
+        bFlag_DoneA = null;
+
+        var fullpath_base = Path.GetExtension(fullpath_all[fullpath_N]);
+        fullpath_base = fullpath_base.toUpperCase();
+
+        // skip .sfk
+        if (fullpath_base == ".SFK") {
+            continue;
+        }
+        // skip .bak
+        if (fullpath_base == ".BAK") {
+            continue;
+        }
+        // skip .sfap0
+        if (fullpath_base == ".SFAP0") {
+            continue;
+        }
+        // skip .sfvp0
+        if (fullpath_base == ".SFVP0") {
+            continue;
+        }
+        // decide for .veg
+        if (fullpath_base == ".VEG") {
+            if (Vegas.Project.FilePath != null) {
+                fullpath_base = Path.GetFileNameWithoutExtension(fullpath_all[fullpath_N]);
+                fullpath_base = fullpath_base.toUpperCase();
+                newsGroup = Path.GetFileNameWithoutExtension(Vegas.Project.FilePath);
+                newsGroup = newsGroup.toUpperCase();
+                if (fullpath_base == newsGroup) {
+                    newsGroup = null;
+                    continue;
+                } else {
+                    newsGroup = null;
+                }
+            }
+        }
+
+        fullpath_base = Path.GetFullPath(fullpath_all[fullpath_N]);
+
+        // debug
+            //MessageBox.Show(fullpath_base);
+            //continue;
+        // debug end
+
+        CanOpen(fullpath_base);
+        if (null == bFlag_DoneV) {
+            var postfix = ".v.mp4";
+            TryToConvert('-i "'+fullpath_base+ '" -an -c:v libx264 -preset veryfast -crf 15 -y "'+fullpath_base+postfix+'"');
+            CanOpen(fullpath_base + postfix);
+            if (null == bFlag_DoneV) {
+                Vegas.Project.MediaPool.Remove(fullpath_base + postfix);
+            }
+        }
+        if (null == bFlag_DoneA) {
+            var postfix = ".a.wav";
+            TryToConvert('-i "'+fullpath_base+'" -vn -c:a pcm_s16le -ar 48000 -y "'+fullpath_base+postfix+'"');
+            CanOpen(fullpath_base + postfix);
+            if (null == bFlag_DoneA) {
+                Vegas.Project.MediaPool.Remove(fullpath_base + postfix);
+            }
+        }
+        if (null != tStrmLength) {
+            tEventLoc = tEventLoc + tStrmLength;
+            Vegas.UpdateUI();
+        }
+    }
+}
+
+catch(e) {
+    if (e != "ok") {
+        MessageBox.Show(e);
+    }
+}
 }
 
 function FindTrack(WhichTrack) : Track {
@@ -125,4 +228,97 @@ function CreateGeneratedMedia(ticker_track, presetName) {
   key_frame.MoveBy(moveby2);
 
   return newEvent;
-} 
+}
+
+function CanOpen(f_path) {
+    var mMedia = new Media(f_path);
+    try {
+    for (var sTmpStm in mMedia.Streams) {
+        var newVid = null;
+        var newAud = null;
+        if (null == tStrmLength) {
+            tStrmLength = sTmpStm.Length;
+            if (tStrmLength.ToMilliseconds() == 0) {
+                tStrmLength = Timecode.FromMilliseconds(3000);
+            }
+        }
+        if (null == newsGroup) {
+            newsGroup = new TrackEventGroup();
+            Vegas.Project.Groups.Add(newsGroup);
+        }
+
+        if (sTmpStm.MediaType == MediaType.Video) {
+            newVid = new VideoEvent(tEventLoc, tStrmLength);
+            while (1) {
+                //MessageBox.Show("vid");
+                for (var tTmpTrk in Vegas.Project.Tracks) {
+                    if (tTmpTrk.IsVideo()) {
+                        bFlag_DoneV = 1;
+                        tTmpTrk.Events.Add(newVid);
+                        newVid.Takes.Add(new Take(sTmpStm));
+                        TrackEvent(newVid).Loop = null;
+                        newVid.ResampleMode = "Disable";
+                        //newVid.ReduceInterlace = null;
+                        newsGroup.Add(newVid);
+                        break;
+                    }
+                }
+                if (bFlag_DoneV == 1) {
+                    break;
+                } else {
+                    Vegas.Project.Tracks.Add(new VideoTrack(Project.ActiveProject, Vegas.Project.Tracks.Count, ""));
+                }
+            }
+        } else if (sTmpStm.MediaType == MediaType.Audio) {
+            newAud = new AudioEvent(tEventLoc, tStrmLength);
+            while (1) {
+                //MessageBox.Show("aud");
+                for (var tTmpTrk in Vegas.Project.Tracks) {
+                    if (tTmpTrk.IsAudio()) {
+                        bFlag_DoneA = 1;
+                        tTmpTrk.Events.Add(newAud);
+                        newAud.Takes.Add(new Take(sTmpStm));
+                        TrackEvent(newAud).Loop = null;
+                        newsGroup.Add(newAud);
+                        break;
+                    }
+                }
+                if (bFlag_DoneA == 1) {
+                    break;
+                } else {
+                    Vegas.Project.Tracks.Add(new AudioTrack(Project.ActiveProject, Vegas.Project.Tracks.Count, ""));
+                }
+            }
+        }
+    }
+    }
+    catch(e) {
+        Vegas.Project.MediaPool.Remove(f_path);
+    }
+}
+
+function TryToConvert(sArgs) {
+    var prog1 = new System.Diagnostics.Process();
+    prog1.StartInfo.UseShellExecute = false;
+	prog1.StartInfo.RedirectStandardOutput = false;
+    prog1.StartInfo.FileName = sFFMpegPath;
+    prog1.StartInfo.Arguments = sArgs;
+    prog1.Start();
+    prog1.WaitForExit();
+}
+
+function GetVideoDialog() {
+    var openFileDialog = new OpenFileDialog();
+    openFileDialog.Filter = "Любые видео (*.*)|*.*";
+    openFileDialog.Title = "Укажите файл(ы) видео";
+    openFileDialog.Multiselect = true;
+    var initialDir2 = Path.GetDirectoryName(Vegas.Project.FilePath);
+    if (Directory.Exists(initialDir2)) {
+        openFileDialog.InitialDirectory = initialDir2;
+    }
+    if (System.Windows.Forms.DialogResult.OK == openFileDialog.ShowDialog()) {
+        return openFileDialog.FileNames;
+    } else {
+        return null;
+    }
+}
